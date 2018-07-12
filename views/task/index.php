3c12f306785a2e9729f88d9f772d82e5df101659
razumforce <razum.force@gmail.com>
2018-07-12 16:40:30 +0300

    DZ7 - передал новые классы в CalendarWidget

--- views/task/index.php
+++ views/task/index.php
@@ -9,5 +9,10 @@ use app\widgets\CalendarWidget;
 <?php echo $this->render('_calendarperiod', ['modelPeriod' => $modelPeriod]); ?>
 
 <?= CalendarWidget::widget([
+  'cssTable' => 'table table-bordered',
+  'cssRow' => 'td-date',
+  'cssCell' => 'label label-primary', // изменил с label-success на label-primary
+  'cssCellEvent' => 'td-event',
+  'cssEvent' => 'small',
   'tasks' => $tasks
 ]); ?>
