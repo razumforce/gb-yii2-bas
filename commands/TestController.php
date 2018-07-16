d2bf6dbeb9fa72d836dbb3fc9f9160e9d6e5c9f4
razumforce <razum.force@gmail.com>
2018-07-16 17:01:16 +0300

    DZ8 - повторение урока. консоль + форматирование вывода

---
+++ commands/TestController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use yii\helpers\Console;
+
+
+class TestController extends Controller
+{
+    public $url;
+    public $title;
+
+    public function options($actionID)
+    {
+        return array_merge(parent::options($actionID), [
+            'url',
+            'title'
+        ]);
+    }
+
+    public function optionAliases()
+    {
+        return array_merge(parent::optionAliases(), [
+            'u' => 'url',
+            't' => 'title'
+        ]);
+    }
+
+    public function actionIndex($name = '-')
+    {
+        echo 'User name: ' . $name . PHP_EOL;
+        echo 'Test works' . PHP_EOL;
+
+        return ExitCode::OK;
+    }
+
+    public function actionInfo($message = '***')
+    {
+        $this->stdout('Полное форматирование: ' . $message, Console::FG_GREEN);
+        // echo 'Message: ' . $message . PHP_EOL;
+        $urlFormat = $this->ansiFormat($this->url, Console::BG_YELLOW);
+        echo PHP_EOL . 'URL (parameter) = ' . $urlFormat . PHP_EOL;
+        echo 'TITLE (parameter) = ' . $this->title . PHP_EOL;
+        echo 'Info works' . PHP_EOL;
+
+        return ExitCode::OK;
+    }
+}
